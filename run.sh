994c6c1a764e8b1427cd28740d4888b65a11ee4f
Christian Fritz <christian@savioke.com>
2016-08-01 14:14:06 +0200

    Fixed run script

--- run.sh
+++ run.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 tmux start-server
-tmux new-session -d -s savidev
+tmux new-session -d -s serversync
 
 # Screen 0: a
-tmux rename-window -t savidev:0 Master-a
-tmux send-keys -t savidev:0 "cd a && meteor" C-m
+tmux rename-window -t serversync:0 Master-a
+tmux send-keys -t serversync:0 "cd a && meteor" C-m
 
 # Screen 1: c
-tmux new-window -t savidev:1
-tmux rename-window -t savidev:1 'Slave(c)'
-tmux send-keys -t savidev:1 "cd c && meteor -p 3100" C-m
+tmux new-window -t serversync:1
+tmux rename-window -t serversync:1 'Slave(c)'
+tmux send-keys -t serversync:1 "cd c && meteor -p 3100" C-m
 
-tmux select-window -t savidev:0
+tmux select-window -t serversync:0
 
 # attach
 tmux attach
