5a981b8bc81a8e89c153f33c136ab555c962f692
Christian Fritz <christian.fritz@gmail.com>
2016-08-27 12:02:32 -0700

    Trying slow heartbeat
    - works fine for client, but the server has its own heartbeat that is
      set in `new DDP.Server`, i.e., something we can't change per
      connection

--- a/server/main.js
+++ a/server/main.js
@@ -5,15 +5,15 @@ import { Meteor } from 'meteor/meteor';
 Meteor.startup(() => {
   // code to run on server at startup
   console.log(Items.find().count());
-  Meteor.publish("items", function(date) {
-    console.log("publish", date, Items.find().count());
+  Meteor.publish("items", function(id) {
+    console.log("publish", id, Items.find().count());
     // if a date is given it is interpreted as a "minimum" date, only
     // newer items shown
-    if (date) {
-      return Items.find({date1: {$gt: date}});
-    } else {
+    // if (date) {
+    //   return Items.find({date1: {$gt: date}});
+    // } else {
       return Items.find();
-    }
+    // }
   });
 });
 
